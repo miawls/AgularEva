a43adf62a4bdc6ed91a1ed8b424ac6a462f304e4
mia <saulperez.sistemas@gmail.com>
2020-11-25 13:30:35 -0600

    Se agregaron los modulos complemtarios formulario ,lista de empleado y nadvar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {EmpleadoService} from './list-all-data/empleado.service';
 import { AppComponent } from './app.component';
+import { ListAllDataComponent } from './list-all-data/list-all-data.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { FomularioClienteComponent } from './fomulario-cliente/fomulario-cliente.component';
+import {FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { NavBComponent } from './nav-b/nav-b.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ListaEmpleados', pathMatch: 'full' },
+  { path: 'ListaEmpleados', component: ListAllDataComponent },
+  { path: 'addEmpleado', component: FomularioClienteComponent },
+  { path: 'Update', component: FomularioClienteComponent },
+  { path: 'delete', component: ListAllDataComponent }
+  //{ path: 'clientes/form', component: FormComponent },
+//  { //path: 'clientes/form/:id', component: FormComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListAllDataComponent,
+    FomularioClienteComponent,
+    NavBComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [EmpleadoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
